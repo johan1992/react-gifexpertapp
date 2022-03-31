a5e05e2ae8f47d5eb7f8539ebd5610102f47f343
Johan Rincon <jxhxn1992@gmail.com>
2022-03-31 17:44:58 -0500

    docs agregado

---
+++ docs/precache-manifest.a79a367e9da61c5ea6c4f213b3a78999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae060218bc2345b35c345790a1fd8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f03537034c5bae660356",
+    "url": "/static/css/main.aead3581.chunk.css"
+  },
+  {
+    "revision": "e107496e2e01402b74e7",
+    "url": "/static/js/2.b11609f6.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.b11609f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03537034c5bae660356",
+    "url": "/static/js/main.5f271048.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
